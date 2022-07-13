f9aa002d22847f43ec1313d1ba453aadcd57ecb5
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-07-13 08:33:46 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>A very reliable and creative developer who sacrifices all for better solutions</p>
+      <img src={image} alt="I made this"/>
+    </div>
+  );
 }
 
 export default About;
